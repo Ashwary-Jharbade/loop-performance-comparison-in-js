02662fdce00983c38ea2b4b5589af664f459993a
Ashwary <ashwary.jharbade999@gmail.com>
2021-10-04 19:43:07 +0530

    Final commit

--- assets/js/forOfLoop.js
+++ assets/js/forOfLoop.js
@@ -4,17 +4,9 @@ const obj = new PerformanceCalculator();
 
 obj.start();
 let sum = 0;
-for(let i in data) {
-    sum = sum + Number(i);
+for(let i of data) {
+    sum = sum + i;
 }
 obj.end();
-console.log(sum); // o/p - string
-console.log(obj.timeDiff); // o/p - 9.782800000011921
-
-// Type conversion (parseInt) result
-// o/p - 49999995000000
-// o.p - 8.704199999988079
-
-// Type conversion (Number) result
-// o/p - 49999995000000
-// o.p - 6.338199999988079
+console.log(sum); // o/p - 49999995000000
+console.log(obj.timeDiff); // o/p - 6.2085
